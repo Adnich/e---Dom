4a650fe782b24b1d85cd95806e4c114231b8e864
Imana Purić <imana.puric.23@size.ba>
2025-12-07 18:23:54 +0100

    Konekcija na bazu
    Konekcija na bazu

---
+++ E-dom/src/main/java/util/DBConnection.java
@@ -0,0 +1,17 @@
+package org.example.edom.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    // Aiven cloud podaci
+    private static final String URL = "jdbc:mysql://mysql-1be47ebf-edom.c.aivencloud.com:13425/defaultdb?sslMode=REQUIRED&allowPublicKeyRetrieval=true";
+    private static final String USERNAME = "avnadmin";
+    private static final String PASSWORD = "AVNS_MBksTmnZOfyGMypiW78";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
